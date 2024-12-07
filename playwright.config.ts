66b8b386303980ac6ad2c2e0d5e1fd1652717be0
mosta <mostafa_mohab18@hotmail.com>
2024-12-07 02:38:07 +0200

    Add Updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { devices ,defineConfig} from '@playwright/test';
 
 export default defineConfig ({
     workers: 8, // Increase the number of workers for parallel execution
-    testDir: './test',
+    testDir: './src/tests',
     timeout: 35 * 1000,
     use: {
         baseURL: "https://www.automationexercise.com",
@@ -19,17 +19,6 @@ export default defineConfig ({
 
     projects: [
         {
-          name: 'chrome',
-          use: { ...devices['Desktop Chrome'] },
-        },
-        {
-          name: 'firefox',
-          use: { ...devices['Desktop Firefox'] },
-        },
-        {
-          name: 'MicrosoftEdge',
-          use: { ...devices['Desktop Edge'] },
-        },{
           name: 'chromium',
           use: { ...devices['Desktop Chrome'] },
         },
