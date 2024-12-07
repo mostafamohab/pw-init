6afee742ee6a5830be26733c51da629c71481128
mosta <mostafa_mohab18@hotmail.com>
2024-12-07 02:47:43 +0200

    Add Updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,10 +3,11 @@ import { devices ,defineConfig} from '@playwright/test';
 export default defineConfig ({
     workers: 8, // Increase the number of workers for parallel execution
     testDir: './src/tests',
-    timeout: 35 * 1000,
+    timeout: 60 * 1000,
     use: {
         baseURL: "https://www.automationexercise.com",
         headless: false,
+        video: 'on',  // Record video during tests
         trace: 'on', // This will generate a trace for every test
         screenshot: 'only-on-failure',
         launchOptions: {
